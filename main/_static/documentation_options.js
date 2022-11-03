d47ff45fc22e8464cdbbacdf3dfc565691b311e2
jrodan <jrodan@users.noreply.github.com>
2022-11-03 12:08:44 +0000

    deploy: ef83d6fe2d34897b7701e14ad3ebcd63da36f578

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev730+ga4f5237',
+    VERSION: '0.1.dev731+gef83d6f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
